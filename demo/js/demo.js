ebdbf52b041ad2bd8310f289c302a932695cb34e
Jordan Santell <jsantell@gmail.com>
2012-03-24 14:14:20 -0400

    Update path to audio

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,6 +1,6 @@
 (function() {
     
-  var audio = new Dance( (new Audio()).src = "../lib/setmeonfire.ogg" );
+  var audio = new Dance( (new Audio()).src = "lib/setmeonfire.ogg" );
   audio.onBeat( 0, 50, function( mag ) {
     console.log( "On beat!", mag );
   }, function( mag ) {
