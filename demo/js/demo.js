22d021289b7e32d21a93fca155c66ea5cde0521a
Jordan Santell <jsantell@gmail.com>
2012-03-24 14:07:24 -0400

    More init

---
+++ demo/js/demo.js
@@ -0,0 +1,10 @@
+(function() {
+    
+  var audio = new Dance( (new Audio()).src = "../lib/setmeonfire.ogg" );
+  audio.onBeat( 0, 50, function( mag ) {
+    console.log( "On beat!", mag );
+  }, function( mag ) {
+    console.log( mag );
+  });
+
+})();
--- src/dance.js
+++ src/dance.js
@@ -1,1 +1,47 @@
-var Dance = {};
+(function() {
+
+  var Dance = function ( audio ) {
+    this.audio = audio;
+    this.channels = this.rate = this.frameBufferLength = null;
+    init.apply( this );
+  }
+
+  Dance.prototype = {
+    onBeat : function( freq, sensitivity, onBeatCallback, offBeatCallback ) {
+      
+    }
+  }
+
+  function init () {
+      console.log(this);
+    var _this = this;
+    _this.audio.addEventListener( 'MozAudioAvailable', function() {
+      audioAvailable.call( _this ); 
+    }, false);
+    _this.audio.addEventListener( 'loadedmetadata', function() {
+      loadedMetadata.call( _this );
+    }, false);
+  }
+
+  function audioAvailable ( e ) {
+    var
+      fb = e.frameBuffer,
+      t  = e.time,
+      signal = new Float32Array( fb.length, this.channels ),
+      magnitude;
+
+    for ( var i = 0, j = this.fbLength / 2; i < j; i++ ) {
+      signal[ i ] = ( fb[ 2 * i ] + fb[ 2 * i + 1 ] ) / 2;
+    }
+    this.fft.forward( signal );
+  }
+
+  function loadedMetadata () {
+    this.fbLength = this.audio.mozFrameBufferLength;
+    this.channels = this.audio.mozChannels;
+    this.rate     = this.audio.mozSampleRate;
+    this.fft      = new FFT( this.fbLength / this.channels, this.rate );
+  }
+
+  window.Dance = Dance;
+})();
