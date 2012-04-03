66175ec8f79650137b9ba715369e1f8e3c442624
Jordan Santell <jsantell@gmail.com>
2012-04-03 00:57:04 -0400

    New demo song and visuals

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,46 +1,51 @@
 (function() {
 
   var
-    dance       = new Dance( "lib/setmeonfire.ogg" ),
+    dance       = new Dance( "lib/zircon_devils_spirit.ogg" ),
     particles   = group.children,
     pLength     = particles.length,
     canvas      = document.getElementsByTagName('canvas')[0],
     ctx         = canvas.getContext('2d'),
-    decayScalar = 3,
+    decayScalar = 2,
     limit       = 40,
-    growScalar  = 200,
+    growScalar  = 250,
     growVector  = new THREE.Vector3(10, 10, 10),
     decayVector = new THREE.Vector3(decayScalar, decayScalar, decayScalar),
     currentSection = 0,
     colors = [ 0xff0077, 0xAAEE22, 0x04DBE5, 0xFFB412 ];
+    colorsStrings = [ '#ff0077', '#AAEE22', '#04DBE5', '#FFB412' ];
 
   dance.play();
 
   dance.after( 0, function() {
-    dance.onBeat( 5, 0.03, function( mag ) {
+    dance.onBeat( 3, 0.05, function( mag ) {
       setSize( mag * growScalar );
     }, function( mag ) {
       decay();
     });
+    dance.onBeat( 0, 0.08, function( mag ) {
+      document.getElementsByTagName('body')[0].style.backgroundColor=colorsStrings[ ~~(Math.random() * 4) ];
+    }, function( mag ) {
+      document.getElementsByTagName('body')[0].style.backgroundColor='#212426';
+    });
   });
 
   dance.onceAt( 0, function() {
-    rotateSpeed = -1;
-  }).between( 13.2, 27.2, function() {
-    if (rotateSpeed < 1) rotateSpeed += 0.1;
-  }).onceAt( 27.2, function() {
     for (i = 0; i < pLength; i++) {
       particles[i].material.color.setRGB(0.9,0.9,0.9);
     }
-  }).between( 27.2, 40.3, function() {
-    rotateSpeed = dance.time() < 40.3 ? rotateSpeed + 0.003 : 0;
-  }).onceAt( 40.3, function() {
-    rotateSpeed = 1.5;
-    growScalar = 400;
-    limit = 60;
+  }).onceAt( 16.2, function() {
+    if (rotateSpeed < 1) rotateSpeed += 0.1;
+  }).between( 16.8, 27.2, function() {
     for (i = 0; i < pLength; i++) {
       particles[i].material.color.setHex( colors[i%4] );
     }
+  }).onceAt( 49, function() {
+    rotateSpeed = 0.5;
+    growScalar = 400;
+    limit = 60;
+  }).onceAt( 66.5, function() {
+      
   });
   
   function setSize ( mag ) {
