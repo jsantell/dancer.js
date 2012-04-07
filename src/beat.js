0096511a9724b431d184b95b81a40f31bbe89b62
Jordan Santell <jsantell@gmail.com>
2012-04-07 12:26:46 -0400

    Add beat prototype

---
+++ src/beat.js
@@ -0,0 +1,27 @@
+(function() {
+  var Beat = function ( dance, freq, threshold, decay, onBeat, offBeat ) {
+    this.dance     = dance;
+    this.freq      = freq;
+    this.threshold = threshold;
+    this.decay     = decay;
+    this.onBeat    = onBeat;
+    this.offBeat   = offBeat;
+    this.isOn      = false;
+    
+    var _this = this;
+    this.dance.bind( 'update', function() {
+      if ( !_this.isOn ) { return; }
+      var magnitude = _this.dance.spectrum()[ _this.freq ];
+      magnitude >= _this.threshold ?
+        onBeat( magnitude ) :
+        offBeat( magnitude );
+    });
+  };
+
+  Beat.prototype = {
+    on  : function () { this.isOn = true; },
+    off : function () { this.isOn = false; }
+  };
+
+  window.Dance.Beat = Beat;
+})();
