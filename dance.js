3c8c24d54e263875a3fd31137b4c05e571c50800
Jordan Santell <jsantell@gmail.com>
2012-04-08 14:10:39 -0400

    Build

--- dance.js
+++ dance.js
@@ -158,22 +158,28 @@
 })();
 
 (function() {
-  var Beat = function ( dance, freq, threshold, decay, onBeat, offBeat ) {
+  var Beat = function ( dance, frequency, threshold, decay, onBeat, offBeat ) {
     this.dance     = dance;
-    this.freq      = freq;
+    this.frequency = frequency;
     this.threshold = threshold;
     this.decay     = decay;
     this.onBeat    = onBeat;
     this.offBeat   = offBeat;
     this.isOn      = false;
-    
+    this.currentThreshold = threshold;
+
     var _this = this;
     this.dance.bind( 'update', function() {
       if ( !_this.isOn ) { return; }
-      var magnitude = _this.dance.spectrum()[ _this.freq ];
-      magnitude >= _this.threshold ?
-        onBeat.call( _this.dance, magnitude ) :
+      var magnitude = _this.dance.spectrum()[ _this.frequency ];
+      if ( magnitude >= _this.currentThreshold &&
+          magnitude >= _this.threshold ) {
+        _this.currentThreshold = magnitude;
+        onBeat.call( _this.dance, magnitude );
+      } else {
         offBeat.call( _this.dance, magnitude );
+        _this.currentThreshold -= _this.decay;
+      }
     });
   };
 
