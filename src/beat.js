e57ac5a376a4bf3c66c7f70e6410849d2be2f088
Jordan Santell <jsantell@gmail.com>
2012-04-27 13:59:09 -0400

    Beat now uses auto detection with option object params

--- src/beat.js
+++ src/beat.js
@@ -1,24 +1,25 @@
 (function() {
-  var Beat = function ( dancer, frequency, threshold, decay, onBeat, offBeat ) {
-    this.dancer     = dancer;
-    this.frequency = frequency;
-    this.threshold = threshold;
-    this.decay     = decay;
-    this.onBeat    = onBeat;
-    this.offBeat   = offBeat;
+  var Beat = function ( dancer, options ) {
+    options = options || {};
+    this.dancer    = dancer;
+    this.range     = options.range     || [ 0, 20 ];
+    this.threshold = options.threshold || 0.15;
+    this.decay     = options.decay     || 0.025;
+    this.onBeat    = options.onBeat;
+    this.offBeat   = options.offBeat;
     this.isOn      = false;
-    this.currentThreshold = threshold;
+    this.currentThreshold = this.threshold;
 
     var _this = this;
     this.dancer.bind( 'update', function() {
       if ( !_this.isOn ) { return; }
-      var magnitude = _this.dancer.getSpectrum()[ _this.frequency ];
+      var magnitude = maxAmplitude( _this.dancer.getSpectrum(), _this.range );
       if ( magnitude >= _this.currentThreshold &&
           magnitude >= _this.threshold ) {
         _this.currentThreshold = magnitude;
-        onBeat.call( _this.dancer, magnitude );
+        _this.onBeat && _this.onBeat.call( _this.dancer, magnitude );
       } else {
-        offBeat.call( _this.dancer, magnitude );
+        _this.offBeat && _this.offBeat.call( _this.dancer, magnitude );
         _this.currentThreshold -= _this.decay;
       }
     });
@@ -28,6 +29,18 @@
     on  : function () { this.isOn = true; },
     off : function () { this.isOn = false; }
   };
+
+  function maxAmplitude ( fft, range ) {
+    var max = 0;
+    // Not robust at all array detection, but fast
+    if ( !range.length ) {
+      return fft[ range ];
+    }
+    for ( var i = range[ 0 ], l = range[ 1 ]; i <= l; i++ ) {
+      if ( fft[ i ] > max ) { max = fft[ i ]; }
+    }
+    return max;
+  }
 
   window.Dancer.Beat = Beat;
 })();
