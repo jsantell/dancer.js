f5d09df704cdc27e58af4923ab4be0074170a565
Jordan Santell <jsantell@gmail.com>
2012-06-14 16:49:02 -0400

    Add Simple FFT example

---
+++ demos/simple_fft/js/player.js
@@ -0,0 +1,64 @@
+(function () {
+
+  window.COLOR = '';
+
+  var
+    AUDIO_FILE = '../songs/zircon_devils_spirit',
+    fft = document.getElementById( 'fft' ),
+    ctx = fft.getContext( '2d' ),
+    dancer, beat;
+
+  /*
+   * Dancer.js magic
+   */
+  Dancer.setOptions({
+    flash: '../../lib/'
+  });
+
+  dancer = new Dancer( AUDIO_FILE, [ 'ogg', 'mp3' ] );
+  beat = dancer.createBeat({
+    onBeat: function () {
+      ctx.fillStyle = '#ff0077';
+    },
+    offBeat: function () {
+      ctx.fillStyle = '#333';
+    }
+  }).on();
+  dancer.fft( fft, { fillStyle: '#333' });
+
+  Dancer.isSupported() || loaded();
+  !dancer.isLoaded() ? dancer.bind( 'loaded', loaded ) : loaded();
+
+  /*
+   * Loading
+   */
+
+  function loaded () {
+    var
+      loading = document.getElementById( 'loading' ),
+      anchor  = document.createElement('A'),
+      supported = Dancer.isSupported(),
+      p;
+
+    anchor.appendChild( document.createTextNode( supported ? 'Play!' : 'Close' ) );
+    anchor.setAttribute( 'href', '#' );
+    loading.innerHTML = '';
+    loading.appendChild( anchor );
+
+    if ( !supported ) {
+      p = document.createElement('P');
+      p.appendChild( document.createTextNode( 'Your browser does not currently support either Web Audio API or Audio Data API. The audio may play, but the visualizers will not move to the music; check out the latest Chrome or Firefox browsers!' ) );
+      loading.appendChild( p );
+    }
+
+    anchor.addEventListener( 'click', function () {
+      dancer.play();
+      document.getElementById('loading').style.display = 'none';
+    }, false );
+
+  }
+
+  // For debugging
+  window.dancer = dancer;
+
+})();
