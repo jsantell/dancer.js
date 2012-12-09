9f539762e283ef1ffda1506a541f144577b45908
Endel Dreyer <endel.dreyer@gmail.com>
2012-12-09 20:08:18 -0200

    Remove deprecated/testing code.

--- examples/fft-microphone/js/player.js
+++ examples/fft-microphone/js/player.js
@@ -53,32 +53,3 @@
   window.dancer = dancer;
 
 })();
-
-window.onload = function () {
-  return;
-
-  function startUserMedia(stream) {
-    input = audio_context.createMediaStreamSource(stream);
-    input.connect(audio_context.destination);
-
-    startDancer(input);
-  }
-
-  try {
-    // webkit shim
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-    window.URL = window.URL || window.webkitURL;
-
-    audio_context = new AudioContext;
-    __log('Audio context set up.');
-    __log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
-  } catch (e) {
-    alert('No web audio support in this browser!');
-  }
-
-  navigator.getUserMedia({audio: true}, startUserMedia, function(e) {
-    __log('No live audio input: ' + e);
-  });
-
-};
